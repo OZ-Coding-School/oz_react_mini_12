5cf24cce8dc77896de8146ffe6b068e040e6d57a
kim-mijin <mycrimosn@naver.com>
2025-07-24 18:12:21 +0900

    feat: 메인 및 상세 페이지 레이아웃, 라우팅 구현

---
+++ react-mini-project/src/components/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import NavBar from './NavBar';
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar /> 
+      <div style={styles.content}>
+        <Outlet /> 
+      </div>
+    </div>
+  );
+};
+
+const styles = {
+  content: {
+    padding: '20px',
+  },
+};
+
+export default Layout;
